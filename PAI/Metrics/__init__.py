75855267489e8a860286ea56f632bac4c89f5658
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-09-17 21:15:12 +0530

    update metrics package imports

--- PAI/Metrics/__init__.py
+++ PAI/Metrics/__init__.py
@@ -1,8 +1,10 @@
+# importing all metric calculation functions
 from .tpi import calculate_tpi
-from .aai import calculate_aai
+from .aai import calculate_aai, calculate_average_study, calculate_average_class
 from .phai import calculate_phai
 from .sri import calculate_sri
-from .abi import calculate_abi
+from .abi import calculate_abi, calculate_average_other_activities
 from .tui import calculate_tui
 from .ei import calculate_ei
 from .dci import calculate_dci
+from .pai import calculate_pai
