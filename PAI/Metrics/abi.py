5bbc5c46990802ab60a4eab9fc57d8b361485fbf
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-09-17 21:15:34 +0530

    add other activities average

--- PAI/Metrics/abi.py
+++ PAI/Metrics/abi.py
@@ -1,21 +1,44 @@
-# calculating average free time
+# calculating the average amount of free or unaccounted time per valid day
 def calculate_abi(valid_days):
 
-    # checking if there are no valid days
+    # returning zero when there are no valid days available for calculation
     if len(valid_days) == 0:
         return 0
 
-    # storing total free time
+    # creating a variable to store the total free time of all valid days
     total_free_time = 0
 
-    # adding free time of each day
+    # adding the free time recorded for each valid day
     for day in valid_days:
 
         free_time = day["free_time"] or 0
 
         total_free_time += free_time
 
-    # calculating average free time
+    # calculating the average free time from the total free time
     abi = total_free_time / len(valid_days)
 
     return abi
+
+
+# calculating the average amount of other activities recorded for each valid day
+def calculate_average_other_activities(valid_days):
+
+    # returning zero when there are no valid days available for calculation
+    if len(valid_days) == 0:
+        return 0
+
+    # creating a variable to store the total other activities time
+    total_other_activities = 0
+
+    # adding other activities time recorded for each valid day
+    for day in valid_days:
+
+        other_activities = day["other_activities"] or 0
+
+        total_other_activities += other_activities
+
+    # calculating the average other activities time per valid day
+    average_other_activities = total_other_activities / len(valid_days)
+
+    return average_other_activities
